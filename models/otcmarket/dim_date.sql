f88cff93e70de708c1de936c182044e7b6f48077
Chantal Villacres <chantal.villacres@baruchmail.cuny.edu>
2024-04-27 15:45:15 +0000

    update date dimension sql

--- models/otcmarket/dim_date.sql
+++ models/otcmarket/dim_date.sql
@@ -6,21 +6,22 @@
 
 
 WITH date_cte AS (
-   SELECT DISTINCT CAST(closingbest_biddate AS timestamp) as date_value FROM otc_market_raw 
+   SELECT DISTINCT CAST(closingbest_biddate AS timestamp) as date_value FROM "otcmarket.hhc390ihqgzwa4hy"
    UNION
-   SELECT DISTINCT CAST(closingbest_askdate AS timestamp) as date_value FROM otc_market_raw 
+   SELECT DISTINCT CAST(closingbest_askdate AS timestamp) as date_value FROM "otcmarket.hhc390ihqgzwa4hy"
 )
 
 
 SELECT
- 
- CAST(EXTRACT (YEAR FROM date_value) AS INTEGER) AS yearNumber,
- EXTRACT (MONTH FROM date_value) AS monthNumber,
- EXTRACT (DAY FROM date_value) AS dayNumber,
+ {{ date_to_yymmdd (date_Value) }} AS date_id, 
+ date_value AS date_iso_format,
+ EXTRACT (YEAR FROM date_value) AS yearNumber,
  EXTRACT (QUARTER FROM date_value) AS quarterNumber,
+ EXTRACT (MONTH FROM date_value) AS monthNumber,
  TO_CHAR(date_value, 'Month') AS monthName,
+ FLOOR (EXTRACT (day FROM date_value) - 1) / 7) + 1 AS weekOfMonth,
+ EXTRACT (week FROM date_value) AS weekOfYear,
+ EXTRACT (DAY FROM date_value) AS dayNumber,
  EXTRACT (isodow FROM date_value) AS dayofWeek,
- FLOOR (EXTRACT (day FROM date_value) - 1) / 7) + 1 AS weekofMonth,
- TO_CHAR(date_value, 'Day') AS dayName,
- EXTRACT (week FROM date_value) AS weekofYear
+ TO_CHAR(date_value, 'Day') AS dayName
 FROM date_cte
