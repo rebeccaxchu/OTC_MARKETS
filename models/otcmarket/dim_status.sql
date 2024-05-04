1399febcb126a7c2223eb557673e47614fab3029
xiashu chu <xiashu.chu@baruchmail.cuny.edu>
2024-05-04 03:43:50 +0000

    update status dim with new status id. improved query performance.

--- models/otcmarket/dim_status.sql
+++ models/otcmarket/dim_status.sql
@@ -4,21 +4,29 @@
    )
 }}
 
-WITH status_cte AS
-(
-   SELECT DISTINCT 
-            status_name
-    FROM public."otcmarket.hhc390ihqgzwa4hy"
-    WHERE status_name IS NOT NULL
-      AND status_name !~ '^[0-9]+(\.[0-9]+)?$'
-      AND status_name !~ '^(\d{4}-\d{2}-\d{2}|\d{2}/\d{2}/\d{4})$'
-      AND status_name IN ('Active', 'Halted', 'Revoked', 'Suspend')
-),
-status_with_id AS (
+WITH StatusMapping AS (
     SELECT 
         status_name,
-        ROW_NUMBER() OVER (ORDER BY status_name) AS status_id
-    FROM status_cte
+        CASE 
+            WHEN status_name = 'Active' THEN 101
+            WHEN status_name = 'Halted' THEN 102
+            WHEN status_name = 'Revoked' THEN 103
+            WHEN status_name = 'Suspend' THEN 104
+            ELSE 999 -- For any unexpected values
+        END AS status_id
+    FROM 
+        public."otcmarket.hhc390ihqgzwa4hy"
+    WHERE 
+        status_name IS NOT NULL AND
+        status_name !~ '^[0-9]+(\.[0-9]+)?$' AND
+        status_name !~ '^(\d{4}-\d{2}-\d{2}|\d{2}/\d{2}/\d{4})$' AND
+        status_name IN ('Active', 'Halted', 'Revoked', 'Suspend')
+    GROUP BY 
+        status_name
 )
-SELECT status_id, status_name
-FROM status_with_id
+
+SELECT 
+    status_id, 
+    status_name
+FROM 
+    StatusMapping
